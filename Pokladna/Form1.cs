239b4fc01e09fee1b4e3ae14ff4ed95ccc0c22e2
Lukáš Kleveta <Kleveta.Lukas@stud.vassboskovice.cz>
2020-11-03 10:46:50 +0100

    SQLdotaz

--- Pokladna/Form1.cs
+++ Pokladna/Form1.cs
@@ -12,6 +12,8 @@ namespace Pokladna
 {
     public partial class Form1 : Form
     {
+        
+
         List<PoklZaznam> pokladna;
         Irepos repositar;
         public Form1()
@@ -21,43 +23,53 @@ namespace Pokladna
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            SqlDotaz sqlDotaz = new SqlDotaz();
             JsonDotaz jsonDotaz = new JsonDotaz("Zaznamy.json");
-            //jsonDotaz.VytvorTestData();
-            repositar = jsonDotaz;
+            repositar = sqlDotaz;
+            //sqlDotaz.vytvorTest(jsonDotaz.NactiVse());
+
+
+
+           
             
 
             comboBoxRok.SelectedIndex = comboBoxRok.Items.IndexOf(DateTime.Now.Year.ToString());
             comboBoxMesic.SelectedIndex = DateTime.Now.Month-1;
+
+           
         }
 
         private void comboBoxRok_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBoxRok.SelectedIndex >= 0 && comboBoxMesic.SelectedIndex >= 0) 
-            {
-                pokladna = repositar.NactiMesic(int.Parse(comboBoxRok.SelectedItem.ToString()),comboBoxMesic.SelectedIndex+1);
-                listView2.Items.Clear();
-                foreach (var p in pokladna)
-                {
-                    listView2.Items.Add(p.DoItem());
-                }
-            }   
+            repositar.NactiVse();
+            //if (comboBoxRok.SelectedIndex >= 0 && comboBoxMesic.SelectedIndex >= 0) 
+            //{
+            //    pokladna = repositar.NactiMesic(int.Parse(comboBoxRok.SelectedItem.ToString()),comboBoxMesic.SelectedIndex+1);
+            //    listView2.Items.Clear();
+            //    foreach (var p in pokladna)
+            //    {
+            //        listView2.Items.Add(p.DoItem());
+            //    }
+            //}   
           
         }
         private void comboBoxMesic_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBoxRok.SelectedIndex >= 0 && comboBoxMesic.SelectedIndex >= 0)
-            {
-                pokladna = repositar.NactiMesic(int.Parse(comboBoxRok.SelectedItem.ToString()), comboBoxMesic.SelectedIndex + 1);
-                listView2.Items.Clear();
-                foreach (var p in pokladna)
-                {
-                    listView2.Items.Add(p.DoItem());
-                }
-            }
+            
+            //if (comboBoxRok.SelectedIndex >= 0 && comboBoxMesic.SelectedIndex >= 0)
+            //{
+            //    pokladna = repositar.NactiMesic(int.Parse(comboBoxRok.SelectedItem.ToString()), comboBoxMesic.SelectedIndex + 1);
+            //    listView2.Items.Clear();
+            //    foreach (var p in pokladna)
+            //    {
+            //        listView2.Items.Add(p.DoItem());
+            //    }
+            //}
         }
         private void textBoxCisloDokladu_TextChanged(object sender, EventArgs e)
         {
             buttonUloz.Enabled = textBoxCisloDokladu.Text != "";
+            
         }
 
         private void textBoxPopisDokladu_TextChanged(object sender, EventArgs e)
--- Pokladna/SqlDotaz.cs
+++ Pokladna/SqlDotaz.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Pokladna
 {
-    public class SqlDotaz : Irepos
+    public class SqlDotaz : Irepos    
     {
+        private string connectString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Pokladna; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public void vytvorTest(List<PoklZaznam>vychoziZaznam) 
+        {
+            using (SqlConnection connect = new SqlConnection(connectString))
+            {
+                string dotaz= "CREATE TABLE [dbo].PokladniZaznam([IdPokladniZaznam] INT NOT NULL PRIMARY KEY IDENTITY(1,1),[CisloDokladu] INT NOT NULL,[Datum] DATETIME NOT NULL,[PopisZaznamu] VARCHAR(150) NOT NULL,[Castka] FLOAT NOT NULL,[Zustatek] FLOAT NOT NULL,[Poznamka] VARCHAR(150) NULL);";
+                foreach (var z in vychoziZaznam) 
+                {
+                    dotaz += $"insert into PokladniZaznam(CisloDokladu, Datum,PopisZaznamu,Castka, Zustatek,Poznamka)" + $"values({z.CisloDokladu},{z.Datum.ToString("yyyyMMdd")},{z.PopisZaznamu},{z.Castka},{z.Zustatek},{z.Poznamka})";
+
+                }
+                using (SqlCommand sqlComm = new SqlCommand(dotaz, connect))
+                {
+                    connect.Open();
+                    sqlComm.ExecuteNonQuery();  
+
+                    connect.Close();
+                }
+                
+            }
+        }
         public List<PoklZaznam> NactiMesic(int rok, int mesic)
         {
             throw new NotImplementedException();
@@ -15,7 +38,26 @@ namespace Pokladna
 
         public List<PoklZaznam> NactiVse()
         {
-            throw new NotImplementedException();
+            List<PoklZaznam> result = new List<PoklZaznam>();
+
+            using (SqlConnection connect = new SqlConnection(connectString))
+            {
+                using (SqlCommand command = new SqlCommand("select * from PokladniZaznam",connect)) 
+                {
+                    connect.Open();
+                    using (SqlDataReader datareader = command.ExecuteReader()) 
+                    {
+                        while (datareader.Read()) 
+                        {
+                            result.Add(new PoklZaznam(Convert.ToInt32(datareader["IdPokladniZaznam"]), Convert.ToInt32(datareader["CisloDokladu"]),Convert.ToDateTime(datareader["Datum"]), datareader["PopisZaznamu"].ToString(),Convert.ToDouble(datareader["Castka"]), Convert.ToDouble(datareader["Zustatek"]), datareader["Poznamka"].ToString()));
+                        }
+                    }
+
+
+                    connect.Close();
+                }
+            }
+            return result;  
         }
 
         public PoklZaznam NactiZaznam(int idPoklZaznam)
